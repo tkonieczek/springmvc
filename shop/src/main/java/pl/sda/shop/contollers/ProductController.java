7eef4c699c54a2d7ee00899ce9e6e4f99647ca9a
tkonieczek <tomekkonieczek@gmail.com>
2021-06-26 10:03:47 +0200

    upload

--- shop/src/main/java/pl/sda/shop/contollers/ProductController.java
+++ shop/src/main/java/pl/sda/shop/contollers/ProductController.java
@@ -1,21 +1,28 @@
 package pl.sda.shop.contollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pl.sda.shop.dao.ProductDAO;
 import pl.sda.shop.model.Product;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
+    @Value("${app.upload.dir:${user.home}}")
+    public String uploadDir;
     private ProductDAO productDAO;
 
     @Autowired
@@ -45,9 +52,21 @@ public class ProductController {
     }
 
     @PostMapping("/newproduct")
-    public String addNewProduct(Product product) {
+    public String addNewProduct(Product product, @RequestParam("imageFile") MultipartFile file) {
         productDAO.addProduct(product);
+        uploadFile(file);
         return "redirect:/products/list";
+
+    }
+    private void uploadFile(MultipartFile file) {
+
+        try {
+            Path copyLocation = Paths
+                    .get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
